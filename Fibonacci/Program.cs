197144dd44024dfd95dfaa61d34819fb860bb2da
yusufali0 <yusufali.bekci@hotmail.com>
2024-04-01 14:48:27 +0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Terim Sayisi Girin");
+            int terim = Convert.ToInt32(Console.ReadLine());
+            int a = 0, b = 1, c;
+            int toplam = 0;
+            Console.WriteLine("Fibonnaci");
+
+
+            for (int i = 0; i < terim; i++)
+            {
+                Console.WriteLine(a + " ");
+                toplam += a;
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            double ortalama=(double)toplam/terim;
+            Console.WriteLine("ortalama= "+ortalama);
+            Console.ReadLine();
+
+           
+        }
+    }
+}
